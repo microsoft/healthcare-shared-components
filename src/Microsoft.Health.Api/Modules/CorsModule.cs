7352b3e5a27be2feb6cb49843b8b0587dd349da0
Brandon Pollett <brandon.pollett@gmail.com>
2020-03-18 14:05:21 -0700

    Moving common items.

---
+++ src/Microsoft.Health.Api/Modules/CorsModule.cs
@@ -0,0 +1,58 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using EnsureThat;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Health.Api.Features.Cors;
+using Microsoft.Health.Extensions.DependencyInjection;
+using Constants = Microsoft.Health.Api.Features.Cors.Constants;
+
+namespace Microsoft.Health.Api.Modules
+{
+    public class CorsModule : IStartupModule
+    {
+        private readonly CorsConfiguration _corsConfiguration;
+
+        public CorsModule(CorsConfiguration corsConfiguration)
+        {
+            EnsureArg.IsNotNull(corsConfiguration, nameof(corsConfiguration));
+            _corsConfiguration = corsConfiguration;
+        }
+
+        internal CorsPolicy DefaultCorsPolicy { get; private set; }
+
+        public void Load(IServiceCollection services)
+        {
+            EnsureArg.IsNotNull(services, nameof(services));
+
+            var corsPolicyBuilder = new CorsPolicyBuilder()
+                .WithOrigins(_corsConfiguration.Origins.ToArray())
+                .WithHeaders(_corsConfiguration.Headers.ToArray())
+                .WithMethods(_corsConfiguration.Methods.ToArray());
+
+            if (_corsConfiguration.MaxAge != null)
+            {
+                corsPolicyBuilder.SetPreflightMaxAge(TimeSpan.FromSeconds(_corsConfiguration.MaxAge.Value));
+            }
+
+            if (_corsConfiguration.AllowCredentials)
+            {
+                corsPolicyBuilder.AllowCredentials();
+            }
+
+            DefaultCorsPolicy = corsPolicyBuilder.Build();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    Constants.DefaultCorsPolicy,
+                    DefaultCorsPolicy);
+            });
+        }
+    }
+}
