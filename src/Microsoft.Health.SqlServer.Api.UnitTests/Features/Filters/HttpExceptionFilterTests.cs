fac04de83f3ef0a25b0bf8472695454d5ae0d5ab
Brandon Pollett <brandon.pollett@gmail.com>
2020-03-23 16:37:04 -0700

    Updates per feedback.

--- src/Microsoft.Health.SqlServer.Api.UnitTests/Features/Filters/HttpExceptionFilterTests.cs
+++ src/Microsoft.Health.SqlServer.Api.UnitTests/Features/Filters/HttpExceptionFilterTests.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Logging.Abstractions;
