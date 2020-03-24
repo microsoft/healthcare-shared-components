e64a71ee14fdfa9aba9a0199fedc0134e722e919
Brandon Pollett <brandon.pollett@gmail.com>
2020-03-24 15:13:06 -0700

    Add e2e schema tests

---
+++ test/Microsoft.Health.SqlServer.Web/Program.cs
@@ -0,0 +1,22 @@
+// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Microsoft.Health.SqlServer.Web
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
---
+++ test/Microsoft.Health.SqlServer.Web/Startup.cs
@@ -0,0 +1,48 @@
+// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Health.Extensions.DependencyInjection;
+using Microsoft.Health.SqlServer.Api.Registration;
+using Microsoft.Health.SqlServer.Features.Schema;
+using Microsoft.Health.SqlServer.Registration;
+using Microsoft.Health.SqlServer.Web.Features.Schema;
+
+namespace Microsoft.Health.SqlServer.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvc(options => { options.EnableEndpointRouting = false; })
+                .AddNewtonsoftJson();
+
+            services.AddSqlServerBase<SchemaVersion>();
+            services.AddSqlServerApi();
+
+            services.Add(provider => new SchemaInformation((int)SchemaVersion.Version1, (int)SchemaVersion.Version2))
+                .Singleton()
+                .AsSelf()
+                .AsImplementedInterfaces();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public virtual void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
