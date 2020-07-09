e9b750b9e1c769451a5bd92cddd24b1150b2c1d0
Ali <67655573+v-shaaal@users.noreply.github.com>
2020-07-09 14:41:35 -0500

    Health Check Feature Shared Components (#63)
    * Moved health check component that is shared between fhir and dicom codes here. With this implementation, user will be able to check the health of different services like sqlserver, blob container, etc.
    
    * Addressed comments.
    
    * Created an extension method for IApplicationBuilder. This method can be called in DICOM and FHIR to register health check response as JSON.
    
    * Fixed small mistake in the namespace.
    
    * Updated CachedHealthCheck class from sealed to public. Also updated Dispose method accordingly.

---
+++ src/Microsoft.Health.Api/Registration/ApplicationBuilderExtensions.cs
@@ -0,0 +1,48 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Newtonsoft.Json;
+
+namespace Microsoft.Health.Api.Registration
+{
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Use health checks (extension method). Register the response as json.
+        /// </summary>
+        /// <param name="app">Application builder instance.</param>
+        /// <param name="healthCheckPathString">Health check path string.</param>
+        public static void UseHealthChecksExtension(this IApplicationBuilder app, string healthCheckPathString)
+        {
+            app.UseHealthChecks(new PathString(healthCheckPathString), new HealthCheckOptions
+            {
+                ResponseWriter = async (httpContext, healthReport) =>
+                {
+                    var response = JsonConvert.SerializeObject(
+                        new
+                        {
+                            overallStatus = healthReport.Status.ToString(),
+                            details = healthReport.Entries.Select(entry => new
+                            {
+                                name = entry.Key,
+                                status = Enum.GetName(typeof(HealthStatus), entry.Value.Status),
+                                description = entry.Value.Description,
+                            }),
+                        });
+
+                    httpContext.Response.ContentType = MediaTypeNames.Application.Json;
+                    await httpContext.Response.WriteAsync(response);
+                },
+            });
+        }
+    }
+}
