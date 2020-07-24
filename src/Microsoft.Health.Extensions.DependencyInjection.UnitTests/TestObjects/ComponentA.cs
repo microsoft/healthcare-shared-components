34c33fae513c74750e2e772cffe66404a2c56c72
Brendan Kowitz <bkowitz@microsoft.com>
2020-07-23 17:42:55 -0700

    Adds IIndex<TService> to resolve a service with metadata

--- src/Microsoft.Health.Extensions.DependencyInjection.UnitTests/TestObjects/ComponentA.cs
+++ src/Microsoft.Health.Extensions.DependencyInjection.UnitTests/TestObjects/ComponentA.cs
@@ -3,8 +3,11 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System.ComponentModel;
+
 namespace Microsoft.Health.Extensions.DependencyInjection.UnitTests.TestObjects
 {
+    [DisplayName("Component A")]
     public class ComponentA : IComponent
     {
         public string Name { get; } = nameof(ComponentA);
