2f12240e70e694268b9585b6e8c042338d295311
Richa Bansal <ribans@microsoft.com>
2020-07-31 15:08:27 -0700

    Refactoring

--- test/Microsoft.Health.SqlServer.Web/Features/Schema/Migrations/BaseSchema.sql
+++ test/Microsoft.Health.SqlServer.Web/Features/Schema/Migrations/BaseSchema.sql
@@ -1,4 +1,4 @@
-﻿-- NOTE: This script is to apply base schema to run the tool.
+﻿-- NOTE: This script is to apply base schema required for the Schema migration tool
 -- Style guide: please see: https://github.com/ktaranov/sqlserver-kit/blob/master/SQL%20Server%20Name%20Convention%20and%20T-SQL%20Programming%20Style.md
 
 /*************************************************************
