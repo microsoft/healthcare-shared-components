64535386227d0ae3a72d16d53e5a8dbe75d659a1
Yazan Al-Alul <yazanal@microsoft.com>
2021-02-18 18:38:40 -0800

    Add SchemaManager.Core.UnitTests project. Add tests for SqlSchemaManager

---
+++ src/Microsoft.Health.SqlServer/Features/Schema/Manager/IBaseSchemaRunner.cs
@@ -0,0 +1,17 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Health.SqlServer.Features.Schema.Manager
+{
+    public interface IBaseSchemaRunner
+    {
+        public Task EnsureBaseSchemaExistsAsync(CancellationToken cancellationToken);
+
+        public Task EnsureInstanceSchemaRecordExistsAsync(CancellationToken cancellationToken);
+    }
+}
