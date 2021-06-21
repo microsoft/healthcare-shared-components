af69cf5660398ffe2980dd31cca6443e1ee7d433
kannanMSFT <83239411+kannanMSFT@users.noreply.github.com>
2021-06-21 11:33:45 -0700

    Add abstractions and common implementations needed to support Events. (#252)
    * Initial version of changes along with unit test for Shared components needed for Publishing events.
    * Added IChangeFeedSink<T> and IChangeFeedSource<T> Microsoft.Health.Abstractions.Data
    * Added all EventGrid related classes into its own nuget Microsoft.Health.EventGrid that includes 
      - EventGridPublisher, EventGridPublisher and EventGridSink

---
+++ src/Microsoft.Health.Abstractions/Data/IChangeFeedSink.cs
@@ -0,0 +1,31 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.Health.Abstractions.Data
+{
+    /// <summary>
+    /// Generic Sink for writing change feed data to any destination.
+    /// </summary>
+    /// <typeparam name="T">Generic data type T to write</typeparam>
+    public interface IChangeFeedSink<in T>
+    {
+        /// <summary>
+        /// Write data to sink.
+        /// </summary>
+        /// <param name="data">Data of type T.</param>
+        /// <returns>Task.</returns>
+        Task WriteAsync(T data);
+
+        /// <summary>
+        /// Write a collection of type T.
+        /// </summary>
+        /// <param name="data">Data of type T.</param>
+        /// <returns>Task.</returns>
+        Task WriteAsync(IReadOnlyCollection<T> data);
+    }
+}
---
+++ src/Microsoft.Health.Abstractions/Data/IChangeFeedSource.cs
@@ -0,0 +1,27 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Health.Abstractions.Data
+{
+    /// <summary>
+    /// Used as the source for change feed Data of type T.
+    /// </summary>
+    /// <typeparam name="T">Records of Type T</typeparam>
+    public interface IChangeFeedSource<T>
+    {
+        /// <summary>
+        /// Read records from underlying source that matches type T.
+        /// </summary>
+        /// <param name="startId">Start Index of records.</param>
+        /// <param name="pageSize">Page Size of records to fetch.</param>
+        /// <param name="cancellationToken">Cancellation Token.</param>
+        /// <returns>IReadOnlyCollection of T.</returns>
+        Task<IReadOnlyCollection<T>> GetRecordsAsync(long startId, int pageSize, CancellationToken cancellationToken);
+    }
+}
