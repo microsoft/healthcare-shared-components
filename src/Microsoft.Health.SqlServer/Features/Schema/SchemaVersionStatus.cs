afa8182e2ed19b5d96e475e0450caf0100b369e9
apurvabhale <apurvabhale@microsoft.com>
2021-07-21 16:47:52 -0700

    Update the Schema Status to lowercase to be consistent with the statues applied when AutomaticUpdates is turned on

--- src/Microsoft.Health.SqlServer/Features/Schema/SchemaVersionStatus.cs
+++ src/Microsoft.Health.SqlServer/Features/Schema/SchemaVersionStatus.cs
@@ -11,8 +11,16 @@ namespace Microsoft.Health.SqlServer.Features.Schema
     [JsonConverter(typeof(StringEnumConverter))]
     public enum SchemaVersionStatus
     {
-        Started,
-        Completed,
-        Failed,
+        // Note - Updating SchemaVersionStatus to lower case to be consistent with the statuses applied when schema AutomaticUpdates is set to true
+
+#pragma warning disable SA1300 // Element should begin with upper-case letter
+        started,
+#pragma warning restore SA1300 // Element should begin with upper-case letter
+#pragma warning disable SA1300 // Element should begin with upper-case letter
+        completed,
+#pragma warning restore SA1300 // Element should begin with upper-case letter
+#pragma warning disable SA1300 // Element should begin with upper-case letter
+        failed,
+#pragma warning restore SA1300 // Element should begin with upper-case letter
     }
 }
