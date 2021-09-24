c07c85fcbd2dcfd4fecdbfc2dc176ee0379b86b1
Julie Lycklama <8868475+jnlycklama@users.noreply.github.com>
2021-09-24 15:22:01 -0700

    Create MonitoredStream to capture the bytes written to a response (#314)
    * Create stream wrapper which reads all of the bytes written to a stream
    * Create header extension to count the bytes on a IHeaderDictionary
    
    Co-authored-by: jnlycklama <julyckla@gmail.com>

---
+++ src/Microsoft.Health.Api/Extensions/HeaderExtensions.cs
@@ -0,0 +1,53 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Text;
+using EnsureThat;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace Microsoft.Health.Api.Extensions
+{
+    public static class HeaderExtensions
+    {
+        private static readonly Encoding HeaderEncoding = Encoding.GetEncoding("ISO-8859-1");
+        public static readonly int HeaderDelimiterByteCount = HeaderEncoding.GetByteCount(": ");
+        public static readonly int HeaderEndOfLineCharactersByteCount = HeaderEncoding.GetByteCount("\r\n");
+
+        public static int GetTotalHeaderLength(this IHeaderDictionary headers)
+        {
+            // Per https://en.wikipedia.org/wiki/List_of_HTTP_header_fields, each header will be of the form
+            // headerKey: headerValues, and terminated by an end-of-line character sequence. The list of headers
+            // will be terminated by another end-of-line character sequence.
+            EnsureArg.IsNotNull(headers, nameof(headers));
+
+            int headerLength = 0;
+            foreach (KeyValuePair<string, StringValues> header in headers)
+            {
+                headerLength += HeaderEncoding.GetByteCount(header.Key)
+                    + HeaderDelimiterByteCount
+                    + GetByteCount(header.Value)
+                    + HeaderEndOfLineCharactersByteCount;
+            }
+
+            headerLength += HeaderEndOfLineCharactersByteCount;
+
+            return headerLength;
+        }
+
+        public static int GetByteCount(StringValues values)
+        {
+            int totalByteCountOfValues = 0;
+
+            foreach (var value in values)
+            {
+                totalByteCountOfValues += HeaderEncoding.GetByteCount(value);
+            }
+
+            return totalByteCountOfValues;
+        }
+    }
+}
