1b03ad348a811bd77e58fa1abf18be8845a56607
jnlycklama <julyckla@gmail.com>
2021-09-24 15:04:00 -0700

    update header to use static encoding

--- src/Microsoft.Health.Api/Extensions/HeaderExtensions.cs
+++ src/Microsoft.Health.Api/Extensions/HeaderExtensions.cs
@@ -13,28 +13,41 @@ namespace Microsoft.Health.Api.Extensions
 {
     public static class HeaderExtensions
     {
-        public static int GetTotalHeaderLength(this IHeaderDictionary headers, Encoding headerEncoding)
+        private static readonly Encoding HeaderEncoding = Encoding.GetEncoding("ISO-8859-1");
+        public static readonly int HeaderDelimiterByteCount = HeaderEncoding.GetByteCount(": ");
+        public static readonly int HeaderEndOfLineCharactersByteCount = HeaderEncoding.GetByteCount("\r\n");
+
+        public static int GetTotalHeaderLength(this IHeaderDictionary headers)
         {
             // Per https://en.wikipedia.org/wiki/List_of_HTTP_header_fields, each header will be of the form
             // headerKey: headerValues, and terminated by an end-of-line character sequence. The list of headers
             // will be terminated by another end-of-line character sequence.
             EnsureArg.IsNotNull(headers, nameof(headers));
-            EnsureArg.IsNotNull(headerEncoding, nameof(headerEncoding));
 
-            int headerDelimiterByteCount = headerEncoding.GetByteCount(": ");
-            int headerEndOfLineCharactersByteCount = headerEncoding.GetByteCount("\r\n");
             int headerLength = 0;
             foreach (KeyValuePair<string, StringValues> header in headers)
             {
-                headerLength += headerEncoding.GetByteCount(header.Key)
-                    + headerDelimiterByteCount
-                    + headerEncoding.GetByteCount(header.Value.ToString())
-                    + headerEndOfLineCharactersByteCount;
+                headerLength += HeaderEncoding.GetByteCount(header.Key)
+                    + HeaderDelimiterByteCount
+                    + GetByteCount(header.Value)
+                    + HeaderEndOfLineCharactersByteCount;
             }
 
-            headerLength += headerEndOfLineCharactersByteCount;
+            headerLength += HeaderEndOfLineCharactersByteCount;
 
             return headerLength;
+        }
+
+        public static int GetByteCount(StringValues values)
+        {
+            int totalByteCountOfValues = 0;
+
+            foreach (var value in values)
+            {
+                totalByteCountOfValues += HeaderEncoding.GetByteCount(value);
+            }
+
+            return totalByteCountOfValues;
         }
     }
 }
