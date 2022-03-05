450268518b5ac3e5eceddea5e4f2d1745958b623
Smitha Saligrama <smithasa@microsoft.com>
2022-03-04 17:54:12 -0800

    Add a unit test

--- src/Microsoft.Health.SqlServer.UnitTests/Features/Client/RetrySqlCommandWrapperTests.cs
+++ src/Microsoft.Health.SqlServer.UnitTests/Features/Client/RetrySqlCommandWrapperTests.cs
@@ -3,6 +3,8 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using Microsoft.Data.SqlClient;
+using Microsoft.Health.SqlServer.Features.Client;
 using Xunit;
 
 namespace Microsoft.Health.SqlServer.UnitTests.Features.Client
@@ -12,11 +14,15 @@ namespace Microsoft.Health.SqlServer.UnitTests.Features.Client
         [Fact]
         public void GivenASqlCommandWrapper_ItSRetryPolicy_IsExponentialRetry()
         {
-        }
+            var retryOption = SqlConfigurableRetryFactory.CreateExponentialRetryProvider(new SqlRetryLogicOption
+            {
+                NumberOfTries = 3,
+            });
+            var sqlCommandWrapper = new RetrySqlCommandWrapper(
+                                        new SqlCommandWrapper(new Data.SqlClient.SqlCommand()),
+                                        retryOption);
 
-        [Fact]
-        public void GivenASqlConnectionWrapper_ItSRetryPolicy_IsExponentialRetry()
-        {
+            Assert.True(sqlCommandWrapper.RetryLogicProvider == retryOption);
         }
     }
 }
