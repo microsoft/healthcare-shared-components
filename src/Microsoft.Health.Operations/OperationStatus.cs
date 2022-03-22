9f6a88f1d0fc220015838156d54c7361c8c2f17b
Will Sugarman <will.sugarman@microsoft.com>
2022-03-22 12:01:53 -0700

    Rename classes

--- src/Microsoft.Health.Operations/OperationStatus.cs
+++ src/Microsoft.Health.Operations/OperationStatus.cs
@@ -3,57 +3,40 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-
 namespace Microsoft.Health.Operations;
 
 /// <summary>
-/// Represents the metadata for a long-running DICOM operation.
+/// Specifies the status of a DICOM operation.
 /// </summary>
-/// <typeparam name="T">The type used to denote the category of operation.</typeparam>
-public class OperationStatus<T>
+public enum OperationStatus
 {
     /// <summary>
-    /// Gets or sets the operation ID.
+    /// Specifies a state that is missing or unrecognized.
     /// </summary>
-    /// <value>The unique ID that denotes a particular operation.</value>
-    public Guid OperationId { get; init; }
+    Unknown,
 
     /// <summary>
-    /// Gets or sets the category of the operation.
+    /// Specifies a state where execution is pending.
     /// </summary>
-    public T? Type { get; init; }
+    NotStarted,
 
     /// <summary>
-    /// Gets or sets the date and time the operation was started.
+    /// Specifies a state where the operation is executing and has not yet finished.
     /// </summary>
-    /// <value>The <see cref="DateTime"/> when the operation was started.</value>
-    public DateTime CreatedTime { get; init; }
+    Running,
 
     /// <summary>
-    /// Gets or sets the last date and time the operation's execution status was updated.
+    /// Specifies a state where the operation has finished successfully.
     /// </summary>
-    /// <value>The last <see cref="DateTime"/> when the operation status was updated.</value>
-    public DateTime LastUpdatedTime { get; init; }
+    Completed,
 
     /// <summary>
-    /// Gets or sets the execution status of the operation.
+    /// Specifies a state where the operation has stopped prematurely after encountering one or more errors.
     /// </summary>
-    public OperationRuntimeStatus Status { get; init; }
+    Failed,
 
     /// <summary>
-    /// Gets the percentage of work that has been completed by the operation.
+    /// Specifies a state where the operation that has stopped prematurely after being told to do so by the user.
     /// </summary>
-    /// <value>An integer ranging from 0 to 100.</value>
-    public int PercentComplete { get; init; }
-
-    /// <summary>
-    /// Gets the optional collection of resources locations that the operation is creating or manipulating.
-    /// </summary>
-    /// <remarks>
-    /// The set of resources may change until the <see cref="Status"/> indicates completion.
-    /// </remarks>
-    /// <value>A collection of resource IDs, or <see langword="null"/> if there are no targeted resources.</value>
-    public IReadOnlyCollection<Uri>? Resources { get; init; }
+    Canceled,
 }
