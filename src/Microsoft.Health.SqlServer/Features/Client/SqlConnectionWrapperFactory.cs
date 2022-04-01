078088f250f6988c871d6dc866c80c0ededd9b1e
Matt Serdar <99214729+v-mserdar@users.noreply.github.com>
2022-04-01 16:09:37 -0700

    Revising so that a SqlCommand CommandTimeout value can be passed in through a configuration.

--- src/Microsoft.Health.SqlServer/Features/Client/SqlConnectionWrapperFactory.cs
+++ src/Microsoft.Health.SqlServer/Features/Client/SqlConnectionWrapperFactory.cs
@@ -7,6 +7,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using EnsureThat;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using Microsoft.Health.SqlServer.Configs;
 using Microsoft.Health.SqlServer.Features.Storage;
 
 namespace Microsoft.Health.SqlServer.Features.Client;
@@ -16,16 +18,19 @@ public class SqlConnectionWrapperFactory
     private readonly SqlTransactionHandler _sqlTransactionHandler;
     private readonly ISqlConnectionBuilder _sqlConnectionBuilder;
     private readonly SqlRetryLogicBaseProvider _sqlRetryLogicBaseProvider;
+    private readonly SqlServerDataStoreConfiguration _sqlServerDataStoreConfiguration;
 
     public SqlConnectionWrapperFactory(
         SqlTransactionHandler sqlTransactionHandler,
         ISqlConnectionBuilder sqlConnectionBuilder,
-        SqlRetryLogicBaseProvider sqlRetryLogicBaseProvider)
+        SqlRetryLogicBaseProvider sqlRetryLogicBaseProvider,
+        IOptions<SqlServerDataStoreConfiguration> sqlServerDataStoreConfiguration)
     {
         EnsureArg.IsNotNull(sqlTransactionHandler, nameof(sqlTransactionHandler));
         EnsureArg.IsNotNull(sqlConnectionBuilder, nameof(sqlConnectionBuilder));
         EnsureArg.IsNotNull(sqlRetryLogicBaseProvider, nameof(sqlRetryLogicBaseProvider));
 
+        _sqlServerDataStoreConfiguration = EnsureArg.IsNotNull(sqlServerDataStoreConfiguration?.Value, nameof(sqlServerDataStoreConfiguration));
         _sqlTransactionHandler = sqlTransactionHandler;
         _sqlConnectionBuilder = sqlConnectionBuilder;
         _sqlRetryLogicBaseProvider = sqlRetryLogicBaseProvider;
@@ -33,7 +38,7 @@ public class SqlConnectionWrapperFactory
 
     public async Task<SqlConnectionWrapper> ObtainSqlConnectionWrapperAsync(CancellationToken cancellationToken, bool enlistInTransaction = false)
     {
-        SqlConnectionWrapper sqlConnectionWrapper = new SqlConnectionWrapper(_sqlTransactionHandler, _sqlConnectionBuilder, _sqlRetryLogicBaseProvider, enlistInTransaction);
+        SqlConnectionWrapper sqlConnectionWrapper = new SqlConnectionWrapper(_sqlTransactionHandler, _sqlConnectionBuilder, _sqlRetryLogicBaseProvider, enlistInTransaction, _sqlServerDataStoreConfiguration);
         await sqlConnectionWrapper.InitializeAsync(cancellationToken);
 
         return sqlConnectionWrapper;
