b4c16d39f21e24ac620a8b25d4add55357943a61
Jared Erwin <jaerwin@microsoft.com>
2022-04-25 17:03:51 -0700

    Throw truncate error on string longer than column max

--- src/Microsoft.Health.SqlServer/Resources.Designer.cs
+++ src/Microsoft.Health.SqlServer/Resources.Designer.cs
@@ -115,7 +115,7 @@ namespace Microsoft.Health.SqlServer {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Value {0} is out of range of expected precision of {1} and scale of {2}.
+        ///   Looks up a localized string similar to Value {0} is out of range of expected precision of {1} and scale of {2}..
         /// </summary>
         internal static string DecimalValueOutOfRange {
             get {
@@ -192,6 +192,15 @@ namespace Microsoft.Health.SqlServer {
         internal static string ScriptNotFound {
             get {
                 return ResourceManager.GetString("ScriptNotFound", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Value of length {0} is too long for column {1} with length {2}.
+        /// </summary>
+        internal static string StringTooLong {
+            get {
+                return ResourceManager.GetString("StringTooLong", resourceCulture);
             }
         }
     }
