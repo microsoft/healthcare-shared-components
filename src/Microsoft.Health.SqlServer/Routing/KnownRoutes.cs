1cd41f11ad198531537d9cd2d86d835765e0626a
Mitchell Fox <v-mitfox@microsoft.com>
2022-05-25 09:15:46 -0400

    Removed unnecessary using.

--- src/Microsoft.Health.SqlServer/Routing/KnownRoutes.cs
+++ src/Microsoft.Health.SqlServer/Routing/KnownRoutes.cs
@@ -4,7 +4,6 @@
 // -------------------------------------------------------------------------------------------------
 
 using System;
-using System.Globalization;
 
 namespace Microsoft.Health.SqlServer.Features.Routing;
 
