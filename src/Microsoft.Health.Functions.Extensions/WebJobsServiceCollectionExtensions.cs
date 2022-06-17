396dc2e1f02ea3a86c3623c9bc40e67dd526201e
Will Sugarman <will.sugarman@microsoft.com>
2022-06-17 16:41:57 -0700

    Add extension method for configuring app insights in functions

---
+++ src/Microsoft.Health.Functions.Extensions/WebJobsServiceCollectionExtensions.cs
@@ -0,0 +1,53 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnsureThat;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// A collection of <see langword="static" /> methods for configuring Azure Functions services.
+/// </summary>
+public static class WebJobsServiceCollectionExtensions
+{
+    /// <summary>
+    /// Configures the application insights telemetry for Azure Functions applications.
+    /// </summary>
+    /// <param name="services">The collection of services.</param>
+    /// <param name="configure">A delegate for modifying the <see cref="TelemetryConfiguration"/> instance.</param>
+    /// <returns>The given <paramref name="services"/> for additional methods to use.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="services"/> or <paramref name="configure"/> is <see langword="null"/>.
+    /// </exception>
+    public static IServiceCollection ConfigureWebJobsTelemetry(this IServiceCollection services, Action<IServiceProvider, TelemetryConfiguration> configure)
+    {
+        EnsureArg.IsNotNull(services, nameof(services));
+        EnsureArg.IsNotNull(configure, nameof(configure));
+
+        services.TryAddEnumerable(
+            ServiceDescriptor.Singleton<ITelemetryModule, TelemetryConfigurationModule>(
+                p => new TelemetryConfigurationModule(c => configure(p, c))));
+
+        return services;
+    }
+
+    private sealed class TelemetryConfigurationModule : ITelemetryModule
+    {
+        private readonly Action<TelemetryConfiguration> _configure;
+
+        public TelemetryConfigurationModule(Action<TelemetryConfiguration> configure)
+            => _configure = EnsureArg.IsNotNull(configure, nameof(configure));
+
+        public void Initialize(TelemetryConfiguration configuration)
+            => _configure.Invoke(configuration);
+    }
+}
