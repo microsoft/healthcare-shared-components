79a38865724a83e117d8f86f94f590dc30d51d6a
brandonpollett <brpoll@microsoft.com>
2022-07-01 14:49:21 -0400

    Throw more specific schema exceptions
    Throws more specific exceptions when instance schema information is not found in the DB.

---
+++ src/Microsoft.Health.SqlServer/Features/Schema/Manager/Exceptions/InstanceSchemaNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace Microsoft.Health.SqlServer.Features.Schema.Manager.Exceptions;
+
+public class InstanceSchemaNotFoundException : SchemaManagerException
+{
+    public InstanceSchemaNotFoundException(string message)
+        : base(message)
+    {
+    }
+
+    public InstanceSchemaNotFoundException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
