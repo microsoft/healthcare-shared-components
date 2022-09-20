d7bf078422a47717cfbf27e6c7d7cd68fa347453
Will Sugarman <will.sugarman@microsoft.com>
2022-09-20 15:22:38 -0700

    Remove .NET 5 support; refactor

--- src/Microsoft.Health.SqlServer/Properties/AssemblyInfo.cs
+++ src/Microsoft.Health.SqlServer/Properties/AssemblyInfo.cs
@@ -3,13 +3,8 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System;
-using System.Resources;
 using System.Runtime.CompilerServices;
 
-[assembly: CLSCompliant(false)]
-[assembly: InternalsVisibleTo("Microsoft.Health.SchemaManager.Core.UnitTests")]
-[assembly: InternalsVisibleTo("Microsoft.Health.SqlServer.UnitTests")]
-[assembly: InternalsVisibleTo("Microsoft.Health.SqlServer.Tests.Integration")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-[assembly: NeutralResourcesLanguage("en-us")]
+[assembly: InternalsVisibleTo("Microsoft.Health.SchemaManager.Core.UnitTests")]
+[assembly: InternalsVisibleTo("Microsoft.Health.SqlServer.Tests.Integration")]
