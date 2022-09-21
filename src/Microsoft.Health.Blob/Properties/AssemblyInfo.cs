7defc1be6fb963c3459906f5b044c8c9c5fde5fc
Will Sugarman <will.sugarman@microsoft.com>
2022-09-20 17:10:33 -0700

    Remove .NET 5 Support (#578)
    - Remove .NET 5 Targets (now that DICOM and FHIR have moved to .NET 6)
    - Refactor dynamic assembly info
    - Update .NET SDK to latest

--- src/Microsoft.Health.Blob/Properties/AssemblyInfo.cs
+++ src/Microsoft.Health.Blob/Properties/AssemblyInfo.cs
@@ -1,13 +1,8 @@
-﻿// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System;
-using System.Resources;
 using System.Runtime.CompilerServices;
 
-[assembly: CLSCompliant(false)]
-[assembly: InternalsVisibleTo("Microsoft.Health.Blob.UnitTests")]
 [assembly: InternalsVisibleTo("Microsoft.Health.Dicom.Tests.Integration")]
-[assembly: NeutralResourcesLanguage("en-us")]
