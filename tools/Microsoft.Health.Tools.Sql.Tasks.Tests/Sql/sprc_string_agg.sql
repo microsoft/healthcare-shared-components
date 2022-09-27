b271e7031807764d20f7ce9f9c68e951ba6dd956
Cheng Chen <chengchen2@microsoft.com>
2022-09-27 09:34:40 +0800

    update stored procedure

--- tools/Microsoft.Health.Tools.Sql.Tasks.Tests/Sql/sprc_string_agg.sql
+++ tools/Microsoft.Health.Tools.Sql.Tasks.Tests/Sql/sprc_string_agg.sql
@@ -2,7 +2,7 @@ CREATE procedure dbo.TestSP
 with execute as 'dbo'
 as 
 set nocount on
-SELECT STRING_AGG (CONVERT(NVARCHAR(max),'abc'), CHAR(13)) WITHIN GROUP (ORDER BY key_ordinal)
+SELECT KeyCols = STRING_AGG (CONVERT(NVARCHAR(max),'abc'), CHAR(13)) WITHIN GROUP (ORDER BY key_ordinal)
 FROM sys.index_columns IC
 
 GO
