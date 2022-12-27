1b4f26e9176eb3b157df37fa9b03e20635221888
brandonpollett <brpoll@microsoft.com>
2022-12-27 11:50:23 -0500

    Update location of AuditAction to be in core. (#642)

--- src/Microsoft.Health.Core/Features/Audit/AuditAction.cs
+++ src/Microsoft.Health.Core/Features/Audit/AuditAction.cs
@@ -3,7 +3,7 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-namespace Microsoft.Health.Api.Features.Audit;
+namespace Microsoft.Health.Core.Features.Audit;
 
 public enum AuditAction
 {
