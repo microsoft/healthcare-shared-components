7abc8e4a084058ca6e93a2f558ea2ea4a5e61ca0
Will Sugarman <will.sugarman@gmail.com>
2023-06-08 16:40:56 -0700

    Revert "Added workload identity access token handler (#733)"
    This reverts commit cdd7066fe061e3ef0ad16f5bd163804eab2ae283.

---
+++ src/Microsoft.Health.SqlServer/IAccessTokenHandler.cs
@@ -0,0 +1,20 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Health.SqlServer;
+
+public interface IAccessTokenHandler
+{
+    /// <summary>
+    /// Get access token for the resource.
+    /// </summary>
+    /// <param name="resource">Resource.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>Task returning token.</returns>
+    Task<string> GetAccessTokenAsync(string resource, CancellationToken cancellationToken = default);
+}
---
+++ src/Microsoft.Health.SqlServer/ManagedIdentityAccessTokenHandler.cs
@@ -0,0 +1,29 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+using EnsureThat;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace Microsoft.Health.SqlServer;
+
+public class ManagedIdentityAccessTokenHandler : IAccessTokenHandler
+{
+    private readonly AzureServiceTokenProvider _azureServiceTokenProvider;
+
+    public ManagedIdentityAccessTokenHandler(AzureServiceTokenProvider azureServiceTokenProvider)
+    {
+        EnsureArg.IsNotNull(azureServiceTokenProvider, nameof(azureServiceTokenProvider));
+
+        _azureServiceTokenProvider = azureServiceTokenProvider;
+    }
+
+    /// <inheritdoc />
+    public Task<string> GetAccessTokenAsync(string resource, CancellationToken cancellationToken)
+    {
+        return _azureServiceTokenProvider.GetAccessTokenAsync(resource, cancellationToken: cancellationToken);
+    }
+}
