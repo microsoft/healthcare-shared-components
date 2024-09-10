ff171b29f6ca318dabede20694c1f19e02cfab36
Julie Lycklama <8868475+jnlycklama@users.noreply.github.com>
2024-09-10 10:20:30 -0700

    add additional error for cmk issues; add tests (#1034)

--- src/Microsoft.Health.Encryption/Customer/Extensions/AzureStorageErrorExtensions.cs
+++ src/Microsoft.Health.Encryption/Customer/Extensions/AzureStorageErrorExtensions.cs
@@ -3,6 +3,8 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
 using Azure;
 
 namespace Microsoft.Health.Encryption.Customer.Extensions;
@@ -10,9 +12,12 @@ namespace Microsoft.Health.Encryption.Customer.Extensions;
 public static class AzureStorageErrorExtensions
 {
     private const string KeyVaultEncryptionNotFoundErrorCode = "KeyVaultEncryptionKeyNotFound";
+    private const string KeyVaultNotFoundErrorCode = "KeyVaultVaultNotFound";
+
+    private static readonly List<string> KeyVaultErrorCodes = new List<string>() { KeyVaultEncryptionNotFoundErrorCode, KeyVaultNotFoundErrorCode };
 
     public static bool IsCMKError(this RequestFailedException rfe)
     {
-        return rfe?.ErrorCode == KeyVaultEncryptionNotFoundErrorCode;
+        return KeyVaultErrorCodes.Exists(value => value.Equals(rfe?.ErrorCode, StringComparison.OrdinalIgnoreCase));
     }
 }
