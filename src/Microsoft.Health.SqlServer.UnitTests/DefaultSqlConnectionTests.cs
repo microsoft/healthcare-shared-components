0b46bbd993a5fcb3e43da9c80fbf4282c8227dd1
Sergey Galuzo <sergal@microsoft.com>
2025-03-10 18:49:45 -0700

    removed using

--- src/Microsoft.Health.SqlServer.UnitTests/DefaultSqlConnectionTests.cs
+++ src/Microsoft.Health.SqlServer.UnitTests/DefaultSqlConnectionTests.cs
@@ -4,7 +4,6 @@
 // -------------------------------------------------------------------------------------------------
 
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Options;
